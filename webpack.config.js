02d1d69d20916bb8cc7b5ba9b7fd2dbed4db60b4
Cameron Sampson <csampson.js@gmail.com>
2017-11-26 02:50:41 -0600

    Basic UI scaffolding

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const BUILD_DIR = path.resolve(__dirname, 'dist')
+const APP_DIR = path.resolve(__dirname, 'src/ui')
+
+const config = {
+  entry: `${APP_DIR}/index.js`,
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${APP_DIR}/index.html`
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
+
+module.exports = config
